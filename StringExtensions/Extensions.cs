323ba22e7ea5d7bc30c89567b5934f351a4381d7
Thomas <brausendorf@mail.de>
2020-06-22 15:03:08 +0200

    Projektdateien hinzufügen.

---
+++ StringExtensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ThomBraExtensions
+{
+    public static class Extensions
+    {
+        public static string ReverseString(this String str)
+        {
+            string s = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                s += str[i];
+            }
+            return s;
+        }
+
+        public static int CountWords(this String str)
+        {
+            string sTemp = Regex.Replace(str, "( ){2,}", " ");
+            return sTemp.Split(new char[] { ' ' }).Length; //, ',', '.', '?', '!' 
+
+        }
+    }
+}
---
+++ TestingApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using ThomBraExtensions;
+using System;
+
+namespace TestingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!".ReverseString());
+
+            Console.WriteLine("Hello World!".CountWords());
+            Console.WriteLine("Hello!   World!".CountWords());
+            Console.WriteLine("Hello, World!".CountWords());
+        }
+    }
+}
