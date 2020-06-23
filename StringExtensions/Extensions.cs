ececd1f3c07ab10115d30640a84a317d9fe3f96a
Thomas <brausendorf@mail.de>
2020-06-23 12:15:26 +0200

    Added "ToMorseCode"-Method. It translates normal Text to Morse-Code.

--- StringExtensions/Extensions.cs
+++ StringExtensions/Extensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 
@@ -52,6 +53,75 @@ namespace ThomBraExtensions
             {
                 return false;
             }
+        }
+
+        public static string ToMorseCode(this String str)
+        {
+            StringBuilder morseStringBuilder = new StringBuilder();
+
+            Dictionary<char, string> morseCharacters = new Dictionary<char, string>()
+            {
+                {'a', ".-"},
+                {'b', "-..."},
+                {'c', "-.-."},
+                {'d', "-.."},
+                {'e', "."},
+                {'f', "..-."},
+                {'g', "--."},
+                {'h', "...."},
+                {'i', ".."},
+                {'j', ".---"},
+                {'k', "-.-"},
+                {'l', ".-.."},
+                {'m', "--"},
+                {'n', "-."},
+                {'o', "---"},
+                {'p', ".--."},
+                {'q', "--.-"},
+                {'r', ".-."},
+                {'s', "..."},
+                {'t', "-"},
+                {'u', "..-"},
+                {'v', "...-"},
+                {'w', ".--"},
+                {'x', "-..-"},
+                {'y', "-.--"},
+                {'z', "--.."},
+                {'0', "-----"},
+                {'1', ".----"},
+                {'2', "..---"},
+                {'3', "...--"},
+                {'4', "....-"},
+                {'5', "....."},
+                {'6', "-...."},
+                {'7', "--..."},
+                {'8', "---.."},
+                {'9', "----."},
+                {'.',".-.-.-"},
+                {',',"--.--"},
+                {'?',"..--.."},
+                {':',"---..."},
+                {';',"-.-.-."},
+                {' ',"-...-"},
+                {'/',"-..-."},
+                {'"',".-..-."},
+            };
+
+            string tempMorseStr = String.Empty;
+            foreach (Char c in str.ToLower())
+            {                
+                //if there is a corresponding character-Key in dictionary
+                if (morseCharacters.TryGetValue(c, out tempMorseStr))
+                {
+                    morseStringBuilder.Append(tempMorseStr);
+                }
+                // if no corresponding key then concat unchanged input character
+                else
+                {
+                    morseStringBuilder.Append(c);
+                }
+            }
+            return morseStringBuilder.ToString();
         }
     }
 }
--- TestingApplication/Program.cs
+++ TestingApplication/Program.cs
@@ -7,15 +7,13 @@ namespace TestingApplication
     {
         static void Main(string[] args)
         {
-
-            //Console.WriteLine("Hello World".GetAscii());
-            //foreach(int x in "Hello World".GetAscii())
-            //{
-            //    Console.WriteLine(x);
-            //}
             //Testing .IsPalindrome-Method
-            string palStr = "Lagertonnennotregal";
-            Console.WriteLine("The word {0} is a palindrome is: {1}", palStr, palStr.IsPallindrome());
+            //string palStr = "Lagertonnennotregal";
+            //Console.WriteLine("The word {0} is a palindrome is: {1}", palStr, palStr.IsPallindrome());
+
+            //Testing .ToMorseCode-Method
+            string morseSourceText = "Hello World!";
+            Console.WriteLine("Morse-Code: {0}", morseSourceText.ToMorseCode());
         }
     }
 }
